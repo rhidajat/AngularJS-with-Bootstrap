18962aee6ef720efa6749a80d82c820c8e910bd4
reesybreezy <r.hidajat@yahoo.com>
2016-03-03 20:25:33 -0800

    Initial commit, very simple page.

---
+++ js/home.js
@@ -0,0 +1,4 @@
+angular.module('angular.bootstrap', ['ngAnimate', 'ui.bootstrap']);
+angular.module('angular.bootstrap').controller('CollapsedCtrl', function ($scope) {
+	$scope.isCollapsed = false;
+});
